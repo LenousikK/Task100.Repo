cceaa90083aa6387514fea15340b518054811e79
LenousikK <liloo-19@yandex.ru>
2019-05-30 01:44:34 +0300

    Task100.Solution is added.

--- src/test/java/LogOutTutByTest.java
+++ src/test/java/LogOutTutByTest.java
@@ -1,13 +1,23 @@
+import io.qameta.allure.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Epic("Allure")
+@Feature("Logout Page")
+@ExtendWith(MyTestWatcher.class)
 public class LogOutTutByTest extends BeforeAfter {
     private static final String LOGIN_USERNAME = "seleniumtests@tut.by";
     private static final String LOGIN_PASSWORD = "123456789zxcvbn";
-    private static final String EXPECTED_TEXT_OF_LABEL_LOGGED_OUT = "Войти";
+    private static final String EXPECTED_TEXT_OF_LABEL_LOGGED_OUT = "ВойтиTEST";
 
     @Test
+    @Description("Verify that User can log out")
+    @TmsLink("LP-1")
+    @Issue("123333333333333")
+    @Story("As a User I can log out")
+    @Severity(SeverityLevel.CRITICAL)
     public void logoutSuccessful() {
         LoginPage loginPage = new LoginPage(driver, wait);
         LoggedInPage loggedInPage = loginPage.login(LOGIN_USERNAME, LOGIN_PASSWORD);
--- src/test/java/LoginToTutByTest.java
+++ src/test/java/LoginToTutByTest.java
@@ -1,13 +1,21 @@
+import io.qameta.allure.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@Epic("Allure")
+@Feature("Login Page")
+@ExtendWith(MyTestWatcher.class)
 public class LoginToTutByTest extends BeforeAfter {
     private static final String LOGIN_USERNAME = "seleniumtests@tut.by";
     private static final String LOGIN_PASSWORD = "123456789zxcvbn";
     private static final String EXPECTED_TEXT_OF_LABEL_LOGGED_USERNAME = "Selenium Test";
 
     @Test
+    @Description("Verify that User can log in")
+    @TmsLink("LP-2")
+    @Issue("123333333333334")
+    @Story("As a User I can log in")
     public void loginSuccessful() {
         LoginPage loginPage = new LoginPage(driver, wait);
         LoggedInPage loggedInPage = loginPage.login(LOGIN_USERNAME, LOGIN_PASSWORD);
